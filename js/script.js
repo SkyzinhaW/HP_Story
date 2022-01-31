5d3cd93fb560efb8a75a91d4d61bcea670fef52d
Swygax <kennykysv@gmail.com>
2022-01-31 02:08:59 -0300

    movido styles.css para css/ e adicionado script sj/script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function changePunctuation(ClassName, value) {
+				var elem = document.getElementByClassName(ClassName);
+				var old-value = elem.style.heigth.value;
+				elem.style.heigth = value;
+}
+
