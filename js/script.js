da6499cd567b0ccd4b3849dca54df532438fea91
swygax <kennykysv@gmail.com>
2022-02-05 13:05:48 -0300

    deletado styles.css, funcionando botoes de progresso da pontuação

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-function changePunctuation(ClassName, value) {
-				var elem = document.getElementByClassName(ClassName);
-				var old-value = elem.style.heigth.value;
-				elem.style.heigth = value;
+function changePunctuation(id) {
+	let	g = document.getElementById(id);
+	changeSize(g)
 }
 
+function changeSize(elem) {
+	let i = 1;
+	var width = 1;
+	var id = setInterval(frame, 10);
+	function frame() {
+		if (width >= 100) {
+			clearInterval(id);
+			i = 0;
+		} else {
+			width++;
+			elem.style.width = width + "%";
+		}
+	}
+}
